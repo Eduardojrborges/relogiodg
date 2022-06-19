785c86bac79be4cdb527e7b349a1c7350f98c842
Eduardojrborges <81242021+Eduardojrborges@users.noreply.github.com>
2022-06-19 20:09:16 -0300

    teste
    teste

---
+++ dsplay-data.js
@@ -0,0 +1,22 @@
+// fontSize hack to test outside DSPLAY
+
+var template = {
+  background: "test/relogio-bg.jpg",
+  barColor: '#FFF',
+  barOpacity: .2,
+  dateColor: '#999',
+  timeColor: '#666'
+};
+
+var config = {
+  loscale: 'en_US'
+};
+
+function init() {
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  document.body.style.fontSize = (h / 360) + 'em';
+}
+
+
+
+//init();
